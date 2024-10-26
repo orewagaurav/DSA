89c42c1186debd3aed6c738e871d2f1cf1549868
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-10-26 11:22:36 +0530

    multi LinkedList

--- twoLinkedList.c
+++ twoLinkedList.c
@@ -29,46 +29,24 @@ void display(struct node *head){
     }
     printf("\n");
 }
-// int main(){
-//     struct node *head1 =NULL;
-//     struct node *head2 =NULL;
-//     int n1,n2,value;
-//     printf("Enter the no. of elements in head1: ");
-//     scanf("%d",&n1);
-//     printf("Enter the elements: ");
-//     for(int i=0;i<n1;i++){
-//         scanf("%d",&value);
-//         create(&head1,value);
-//     }
-//     printf("Enter tne no. of elements in head2: ");
-//     scanf("%d",&n2);
-//     for(int i =0;i<n2;i++){
-//         scanf("%d",&value);
-//         create(&head2,value);
-//     }
-//     display(head1);
-//     display(head2);
-// }
-
 int main(){
-    int n,m,value;
-    printf("Enter the no. of linked list: ");
-    scanf("%d",&n);
-    struct node **head=(struct node**)malloc(n*sizeof(struct node*));
-    for(int i=0;i<n;i++){
-        head[i]=NULL;
+    struct node *head1 =NULL;
+    struct node *head2 =NULL;
+    int n1,n2,value;
+    printf("Enter the no. of elements in head1: ");
+    scanf("%d",&n1);
+    printf("Enter the elements: ");
+    for(int i=0;i<n1;i++){
+        scanf("%d",&value);
+        create(&head1,value);
     }
-    for(int i=0;i<n;i++){
-        printf("Enter the no. of elements in Llist %d: ",i+1);
-        scanf("%d",&m);
-        printf("Enter the elements of linkedList %d: ",i+1);
-        for(int j=0;j<m;j++){
-            scanf("%d",&value);
-            create(&head[i],value);
-        }
+    printf("Enter tne no. of elements in head2: ");
+    scanf("%d",&n2);
+    for(int i =0;i<n2;i++){
+        scanf("%d",&value);
+        create(&head2,value);
     }
-    for(int i =0;i<n;i++){
-        printf("LinkedList %d: ",i+1);
-        display(head[i]);
-    }
+    display(head1);
+    display(head2);
 }
+
