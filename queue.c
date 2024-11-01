1adbb0c6675ca08c0893ca777914f7d4047cd929
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-11-01 21:39:17 +0530

    Queue

--- queue.c
+++ queue.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
 #include <stdbool.h>
 #define MAX 100
+
+int arr[MAX];
 int backInd = -1;
 int frontInd = -1;
 
 bool isEmpty(){
-    if(backInd==MAX-1)
+    if(backInd!=MAX-1)
     return true;
-    
+    else
+    return false;
+}
+void enQueue(int value){
+    bool check = isEmpty();
+    if(check==true){
+        backInd++;
+        arr[backInd]=value;
+    }
+    else
+    printf("Queue is Full!\n");
+}
+void deQueue(){
+    if(backInd!=frontInd){
+        frontInd++;
+    }
+    else
+    printf("stack is Empty!\n");
+}
+void display(){
+    for(int i = frontInd+1;i<=backInd;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int n,value;
+    printf("Enter the no. of elements: ");
+    scanf("%d",&n);
+    printf("Enter the elements: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&value);
+        enQueue(value);
+    }
+    deQueue();
+    display();
 }
