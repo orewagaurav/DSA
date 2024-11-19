5e940928e99978b33c47f8d43166ab1455efaa75
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-11-20 00:50:27 +0530

    DSA

--- knapsack.c
+++ knapsack.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function to swap two elements
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 // Function to sort items based on value/weight ratio
 void sortItems(int values[], int weights[], int n) {
@@ -11,18 +17,21 @@ void sortItems(int values[], int weights[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (ratios[i] < ratios[j]) {
-                double tmp =ratios[i];
-                ratios[i]=ratios[j];
-                ratios[j]=tmp;
-                
+                double tmpRatio = ratios[i];
+                ratios[i] = ratios[j];
+                ratios[j] = tmpRatio;
+
+                swap(&values[i], &values[j]);
+                swap(&weights[i], &weights[j]);
             }
         }
     }
 }
+
 // Function to calculate the maximum total value in the knapsack
 double getMaxValue(int values[], int weights[], int n, int capacity) {
     sortItems(values, weights, n);
-    
+
     double totalValue = 0.0;
     for (int i = 0; i < n && capacity > 0; i++) {
         if (weights[i] <= capacity) {
@@ -33,25 +42,23 @@ double getMaxValue(int values[], int weights[], int n, int capacity) {
             break;
         }
     }
-    
+
     return totalValue;
 }
 
 int main() {
     int n, capacity;
-    
-    // printf("Enter the number of items and the capacity of the knapsack: ");
+
     scanf("%d %d", &n, &capacity);
 
     int values[n], weights[n];
-    
-    // printf("Enter the values and weights of the items: ");
+
     for (int i = 0; i < n; i++) {
         scanf("%d %d", &values[i], &weights[i]);
     }
 
     double maxValue = getMaxValue(values, weights, n, capacity);
     printf("Maximum total value in the knapsack: %.6f\n", maxValue);
-    
+
     return 0;
 }
