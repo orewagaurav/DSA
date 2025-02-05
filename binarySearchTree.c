4b5e029c62fb6852a313605239f1f885620825a7
Gaurav Kumar <gauravkumar47967@gmail.com>
2025-02-05 20:57:12 +0530

    BST

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -25,18 +25,24 @@ void preOrder(struct node *root){
 //       5
 //     /  \
 //     3   6
-//    / \  
-//   1  4
+//    /  \  
+//   1   4
 
 int main(){
-    struct node *p1,*p2,*p3;
-    p1 = createNode(3);
-    p2=createNode(4);
-    p3=createNode(7);
+    struct node *p1,*p2,*p3,*p4,*p5;
+    p1 = createNode(5);
+    p2=createNode(3);
+    p3=createNode(6);
+    p4=createNode(1);
+    p5=createNode(4);
+
     
-    p1->left=p3;
-    p1->right=p2;
+    p1->left=p2;
+    p1->right=p3;
+    p2->left=p4;
+    p2->right=p5;
     printf("Hello world\n");
     
     preOrder(p1);
+    printf("\n");
 }
