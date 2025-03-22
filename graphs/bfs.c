19ee14e441714cfa84865ef6bbf02913688f47c4
orewagaurav <gauravkumar47967@gmail.com>
2025-03-22 11:49:41 +0530

    BFS

--- graphs/bfs.c
+++ graphs/bfs.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define max 10
+#define max 5
 
 int queue[max];
 int front= -1;
@@ -13,4 +13,44 @@ void enqueue(int value){
         front++;
     rear++;
     queue[rear]=value;
+}
+
+int dequeue(){
+    if(front==-1 || front>rear){
+        printf("underflow");
+        return -1;
+    }
+    int value = queue[front];
+    front++;
+    return value;
+}
+
+void bfs(int graph[max][max],int start,int visited[]){
+    visited[start]=1;
+    enqueue(start);
+
+    while(front<=rear){
+        int current = dequeue();
+        printf("%d ",current);
+
+        for(int i=0;i<max;i++){
+            if(graph[current][i]==1 && visited[i]==0){
+                visited[i]=1;
+                enqueue(i);
+            }
+        }
+    }
+}
+
+int main(){
+    int graph[max][max]={
+        {0, 1, 0, 1, 0},
+        {1, 0, 1, 0, 1},
+        {0, 1, 0, 0, 0},
+        {1, 0, 0, 0, 1},
+        {0, 1, 0, 1, 0}
+    };
+    int visited[max]={0};
+    printf("The BFS of the Graph starting from node 0: ");
+    bfs(graph,0,visited);
 }
