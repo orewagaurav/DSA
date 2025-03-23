98bb7db92126c3f3634a54c3d9bff5dbc0103fd0
orewagaurav <gauravkumar47967@gmail.com>
2025-03-23 11:12:42 +0530

    Adjency Matrix

--- graphs/graph-representation-adjMatrics.c
+++ graphs/graph-representation-adjMatrics.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define max 10
-struct node {
-    int vertices;
-    int matrix[max][max];
-};
+#define MAX 5
 
-void initGraph(struct node *root,int vertices){
-    root->vertices=vertices;
-    for(int i=0;i<vertices;i++){
-        for(int j=0;j<vertices;j++){
-            root->matrix[i][j]=0;
+void initGraph(int matrix[MAX][MAX], int vertices) {
+    for (int i = 0; i < vertices; i++) {
+        for (int j = 0; j < vertices; j++) {
+            matrix[i][j] = 0;
         }
     }
 }
-void addEdge(struct node *root,int src,int dst){
-    root->matrix[src][dst]=1;
-    root->matrix[dst][src]=1;
-}
-void display(struct node *root){
-    printf("Adjacency matrix");
-    for(int i=0;i<root->vertices;i++){
-        for(int j=0;j<root->vertices;j++){
-            printf("%d ",root->matrix[i][j]);
-        }
-    }
-}
-int main(){
-    struct node *root = (struct node *)malloc(sizeof(struct node));
-    if (root == NULL) {
-        printf("Memory allocation failed\n");
-        return 1;
-    }
-    initGraph(root,5);
 
-    addEdge(root,0,1);
-    addEdge(root,0,4);
-    addEdge(root,1,2);
-    addEdge(root,1,3);
-    addEdge(root,1,4);
-    addEdge(root,2,3);
-    addEdge(root,3,4);
-    display(root);
+void addEdge(int matrix[MAX][MAX], int src, int dst) {
+    matrix[src][dst] = 1;
+    matrix[dst][src] = 1; 
+}
+
+void display(int matrix[MAX][MAX], int vertices) {
+    printf("Adjacency Matrix:\n");
+    for (int i = 0; i < vertices; i++) {
+        for (int j = 0; j < vertices; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int matrix[MAX][MAX];
+    int vertices = 5;
+    
+    initGraph(matrix, vertices);
+    addEdge(matrix, 0, 1);
+    addEdge(matrix, 0, 4);
+    addEdge(matrix, 1, 2);
+    addEdge(matrix, 1, 3);
+    addEdge(matrix, 1, 4);
+    addEdge(matrix, 2, 3);
+    addEdge(matrix, 3, 4);
+    
+    display(matrix, vertices);
+    return 0;
 }
